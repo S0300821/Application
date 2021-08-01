d8f2620ee09d0194cc1d03bf68a3fa855f82d7dd
S0300821 <S0300821@grimsby.ac.uk>
2021-08-01 20:33:32 +0100

    Version 1

---
+++ ProtoApp/ProtoApp/ProtoApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ProtoApp.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProtoApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
