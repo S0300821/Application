e96af03c09e7e61e8c23fbd7b1aebb42b9c81b35
S0300821 <S0300821@grimsby.ac.uk>
2021-08-04 19:18:25 +0100

    Version-4

--- ProtoApp/ProtoApp/ProtoApp.Android/obj/Debug/110/android/environment.x86_64.s
+++ ProtoApp/ProtoApp/ProtoApp.Android/obj/Debug/110/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"9ca6e41c-83c9-4296-a79e-34b7a44087e7"
+	.asciz	"ec1313eb-1285-4cab-89aa-63e15f4e176e"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
